86efecff2740d328c9c842ab8c127b4d193f3c05
mariisneves <mariana.aneves@outlook.com>
2022-03-21 17:10:46 -0300

    Criação do projeto e componente home

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <>
+            <p>~ hello world ~</p>
+            <h1 className="titulo">Home</h1>
+            <h2>"come home with me"</h2>
+            <h2>"who are you?"</h2>
+            <img src="https://images.forwardcdn.com/image/1300x/right/top/images/cropped/hadestown-0153-1555597867.jpg"
+                alt="Orfeu, Euridice e Hermes do musical Hadestown" className="img" />
+        </>
+    );
+}
+
+export default Home;
